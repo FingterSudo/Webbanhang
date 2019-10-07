266ca2f6a2e04fcb4666670506db8a6afe51e238
FingterSudo <xuanbach96@gmail.com>
2019-10-07 11:58:47 +0700

    commit

---
+++ Web BanHang/Controllers/sdfdsfController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_BanHang.Controllers
+{
+    public class sdfdsfController : Controller
+    {
+        // GET: sdfdsf
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
