69aa1c97ec5eed81ec591157d752e7ebb7dd9510
FingterSudo <xuanbach96@gmail.com>
2019-10-23 18:58:44 +0700

    commit

--- Web BanHang/Controllers/QuanLySanPhamController.cs
+++ Web BanHang/Controllers/QuanLySanPhamController.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Web_BanHang.Models;
+using System.IO;
+using PagedList;
+using PagedList.Mvc;
 namespace Web_BanHang.Controllers
 {
     public class QuanLySanPhamController : Controller
     {
+        private readonly QuanLyBanSachEntities db = new QuanLyBanSachEntities();
         // GET: QuanLySanPham
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
+            int pageSize =9;
+            int pageNumber = page ?? 1;
+            IPagedList sach = db.Saches.OrderBy(n => n.NgayCapNhap).ToPagedList(pageNumber, pageSize);
+            return View(sach);
+        }
+        // thêm mới sản phẩm
+        [HttpGet]
+        public ActionResult ThemMoi()
+        {
+            // đưa dữ liệu vào dropdownlist
+            ViewBag.MaChuDe = new SelectList(db.ChuDes, "MaChuDe", "TenChuDe");
+            ViewBag.MaTacGia = new SelectList(db.TacGias, "MaTacGia", "TenTacGia");
+            ViewBag.MaNXB = new SelectList(db.NhaXuatBans, "MaNXB", "TenNXB");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ThemMoi(Sach sach,HttpPostedFileBase fileupload)
+        {
+            // lưu tên file
+            var fileName = Path.GetFileName(fileupload.FileName);
+            // lưu đường dẫn của file
+            var path = Path.Combine(Server.MapPath("~/HinhAnh"), fileName);
+            // kiem tra hinh anh da ton tai
+            if (System.IO.File.Exists(path))
+            {
+                ViewBag.ThongBao = "Hình ảnh đã tồn tại";
+            }
+            else
+            {
+                fileupload.SaveAs(path);
+            }
+            ViewBag.MaChuDe = new SelectList(db.ChuDes, "MaChuDe", "TenChuDe");
+            ViewBag.MaTacGia = new SelectList(db.TacGias, "MaTacGia", "TenTacGia");
+            ViewBag.MaNXB = new SelectList(db.NhaXuatBans, "MaNXB", "TenNXB");
             return View();
         }
     }
---
+++ Web BanHang/Models/MetaData/SachMetadata.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// using 2 thư viện thiết kế class MetaData
+using  System.Web.DynamicData;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace Web_BanHang.Models 
+{
+    [MetadataType(typeof(SachMetadata))]
+
+    public partial class Sach 
+    {
+        internal sealed class SachMetadata
+        {
+           
+            public int MaSach { get; set; }
+
+            [Required(ErrorMessage = "Vui lòng nhập tên sách này!")]
+            [DisplayName("Tên sách(*)")]
+            [MaxLength]
+            public string TenSach { get; set; }
+
+            [Required(ErrorMessage = "Vui lòng nhập mô tả !")]
+            [DisplayName("Mô tả(*)")]
+            [MaxLength]
+            public string MoTa { get; set; }
+
+            [Required(ErrorMessage = "Vui lòng nhập ảnh bìa!")]
+            [DisplayName("Ảnh bìa(*)")]
+            [StringLength(50)]
+            public string AnhBia { get; set; }
+
+            [Required(ErrorMessage = "Vui lòng nhập giá bán!")]
+            [DisplayName("Giá bán(*)")]
+            public Nullable<decimal> GiaBan { get; set; }
+
+            [DataType(DataType.Date)]
+            [DisplayName("Ngày cập nhập(*)")]
+            [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}",ApplyFormatInEditMode =true)] // định dạng ngày sinh
+            [Required(ErrorMessage = "Vui lòng nhập cập nhập")]
+            public Nullable<System.DateTime> NgayCapNhap { get; set; }
+
+            [Required(ErrorMessage = "Vui lòng nhập trường này")]
+            [DisplayName("Số lượng tồn(*)")]
+            public Nullable<int> SoLuongTon { get; set; }
+          
+            [DisplayName("Mã tác giả(*)")]
+            public Nullable<int> MaTacGia { get; set; }
+           
+            [DisplayName("Mã nhà xuất bản(*)")]
+            public Nullable<int> MaNXB { get; set; }
+            
+            [DisplayName("Mã chủ đề(*)")]
+            public Nullable<int> MaChuDe { get; set; }
+        }
+    }
+}
