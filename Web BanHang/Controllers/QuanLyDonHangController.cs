ada0347fbca199d5bc74141f98db5aca70db1360
FingterSudo <xuanbach96@gmail.com>
2019-11-14 16:16:36 +0700

    hoan thanh tinh tong tien dang fix giao dien

--- Web BanHang/Controllers/QuanLyDonHangController.cs
+++ Web BanHang/Controllers/QuanLyDonHangController.cs
@@ -83,14 +83,24 @@ namespace Web_BanHang.Controllers
             QuanLyBanSachEntities db = new QuanLyBanSachEntities();
             ViewBag.MaNXB = new SelectList(db.NhaXuatBans, "MaNXB", "TenNXB");
              ViewBag.TenSach = GetBookValue(search).ToString();
-            //List<SachDTO> allbook = db.Saches.Where(n => n.TenSach.Contains(search)).Select(n => new SachDTO
-            //{
-            //    TenSach = n.TenSach
-            //}).ToList();
-
-            //return new JsonResult { Data = allbook, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
-             
-            
+            //ViewBag.GiaBan = GetPrice(Convert.ToString(giaban)).ToString();
+            //List<SelectListItem> 
+            List<SelectListItem> statusdelivery = new List<SelectListItem>()
+             {
+                 new SelectListItem(){Text="chưa giao hàng", Value="1"},
+                 new SelectListItem(){Text="đang giao hàng", Value="2"},
+                 new SelectListItem(){Text="đã giao hàng", Value="3"}
+             };
+            ViewBag.GiaoHang = statusdelivery;
+            DonHang dh = new DonHang();
+            if (dh.TinhTrangGiaoHang == 3)
+            {
+                return RedirectToAction("Thanhtoan");
+            }
+            else
+            {
+                dh.TinhTrangThanhToan = false;
+            }
             return View();
         }
         [HttpPost]
@@ -99,67 +109,65 @@ namespace Web_BanHang.Controllers
             
         
             DonHang donHang = new DonHang();
+            var statusDonHang =form["TinhTrang"].ToString();
             donHang.TenKH = form["txtTenKh"].ToString();
             donHang.DiaChi = form["txtDiaChi"].ToString();
             donHang.DiaChiNhanHang = form["txtDiaChiNh"].ToString();
-            donHang.NgayDat = DateTime.Parse(form["txtNgayDat"]);
             donHang.NgayGiao = DateTime.Parse(form["txtNgayGiao"].ToString());
             donHang.EmailKH = form["txtEmail"].ToString();
-            donHang.DienThoaiKH = form["txtDienthoaiKh"].ToString();
             //donHang.TinhTrangGiaoHang = Convert.ToInt32(form["txtGiaohang"]);
-             if(donHang.TinhTrangThanhToan == null)
-            {
-                donHang.TinhTrangThanhToan = false;
-            }
-            else
-            {
-                donHang.TinhTrangThanhToan = bool.Parse(form["txtThanhToan"]);
-            }
-            if (donHang.TinhTrangGiaoHang == null)
-            {
-                donHang.TinhTrangGiaoHang = 0;
-            }
-            else
-            {
-                donHang.TinhTrangGiaoHang= Convert.ToInt32(form["txtGiaohang"]);
-            }
+            
             donHang.TongTien = Convert.ToDecimal(form["txtTongTien"]);
             ChiTietDonHang ctdh = new ChiTietDonHang();
             ctdh.SoLuong = Convert.ToInt32(form["txtSoLuong"].ToString());
             ctdh.DonGia = Convert.ToDecimal (form["txtDonGia"]);
 
             ctdh.Sach.TenSach = form["txtSach"].ToString();
-            ctdh.Sach.MaChuDe = Convert.ToInt32(form["txtMaChuDe"].ToString());
+            ctdh.MaSach = Convert.ToInt32(form["txtMaCD"].ToString());
+            ctdh.MaNXB = Convert.ToInt32(form["txtMaChuDe"].ToString());
             ViewBag.MaNXB = new SelectList(db.NhaXuatBans, "MaNXB", "TenNXB");
+            db.DonHangs.Add(donHang);
+            db.ChiTietDonHangs.Add(ctdh);
+            db.SaveChanges();
             return View(); 
         }
         public JsonResult GetBookValue(string search)
         {
             QuanLyBanSachEntities db = new QuanLyBanSachEntities();
-            List<SachDTO> allbook = db.Saches.Where(n => n.TenSach.Contains(search)).Select(n => new SachDTO
+            List<SachDTO> allbook = db.Saches.Where(n => n.TenSach .Contains(search)).Select(n => new SachDTO
             {
                 TenSach = n.TenSach
             }).ToList();
             return new JsonResult { Data = allbook, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
-        //public DateTime? FormatDate(string obj)
-        //{
-        //    try
-        //    {
-        //        if (!string.IsNullOrEmpty(obj))
-        //        {
-        //            var silit = obj.Split('-');
-        //            var day = int.Parse(silit[2]);
-        //            var month = int.Parse(silit[1]);
-        //            var year = int.Parse(silit[0]);
-        //            return DateTime.ParseExact(string.Format("{2}/{1}/{0}",  day, month, year), "dd/MM/yyyy", null);
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        throw;
-        //    }
-        //    return null;
-        //}
+        public JsonResult  GetPrice (string search)
+        {
+            QuanLyBanSachEntities db = new QuanLyBanSachEntities();
+            var giaban = db.Saches.Where(n => n.TenSach.Contains(search)).Select(x => x.GiaBan).FirstOrDefault();
+            return Json(giaban, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Thanhtoan()
+        {
+            return View();
+        }
+        public DateTime? FormatDate(string obj)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(obj))
+                {
+                    var silit = obj.Split('-');
+                    var day = int.Parse(silit[2]);
+                    var month = int.Parse(silit[1]);
+                    var year = int.Parse(silit[0]);
+                    return DateTime.ParseExact(string.Format("{2}/{1}/{0}", day, month, year), "dd/MM/yyyy", null);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
     }
 }
